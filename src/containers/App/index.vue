be776918f30adee879e069af1749b38ed0fc69ab
SerafimPoch <serpochapinsky@gmail.com>
2018-12-14 21:41:21 +0200

    add test data structure

--- src/containers/App/index.vue
+++ src/containers/App/index.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-  <p>hello</p>
+  <p>{{this.$store.state.data[0].city.name}}</p>
+
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+import {weatherApi} from '../../api/'
 
 export default {
   name: 'App',
+
+ created(){
+    this.$store.dispatch('getDataByCity')
+ 
+ },
+
+ computed: {
+   ...mapGetters(['testData'])
+ }
+
   
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { weatherApi } from "../api";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    test: []
+    data: []
+  },
+  getters: {
+    testData: state => {
+      setTimeout(() => {
+        return state.data[0].city.name;
+      });
+    }
+  },
+  mutations: {
+    getDataApi: async state => {
+      const dataApi = await weatherApi();
+      return state.data.push(dataApi);
+    }
+  },
+  actions: {
+    getDataByCity: context => {
+      context.commit("getDataApi");
+    }
   }
 });
