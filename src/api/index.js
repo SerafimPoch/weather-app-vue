26beaf275890a2b86eb587f7c8c502b6a63c5053
SerafimPoch <serpochapinsky@gmail.com>
2018-12-16 18:03:29 +0200

    add basic data

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const testData =
   "https://api.openweathermap.org/data/2.5/forecast?q=Kiev&APPID=e2a4d8b55c5d115b04b00a5b2b95c1c9";
 
 export const weatherApi = async () => {
-  return await fetch(testData)
-    .then(data => data.json())
-    .catch(error => console.log(error));
+  const response = await fetch(testData);
+  const data = await response.json();
+  return data;
 };
