4819bb9b82c3339ef35c41419959cdfd0384a515
SerafimPoch <serpochapinsky@gmail.com>
2018-12-24 18:42:54 +0200

    some fixings

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ const key = "&APPID=e2a4d8b55c5d115b04b00a5b2b95c1c9";
 
 export const weatherApi = async (city = "Kiev") => {
   const response = await fetch(url + city + key);
-
   const data = await response.json();
   return data;
 };
